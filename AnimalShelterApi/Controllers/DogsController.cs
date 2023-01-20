0924dc54b2e0b755cde8519b50020d67c99dab0f
DavidDGamble <daviddgamble@gmail.com>
2023-01-20 09:50:46 -0800

    Add Contollers to project.

---
+++ AnimalShelterApi/Controllers/DogsController.cs
@@ -0,0 +1,111 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AnimalShelterApi.Models;
+
+namespace AnimalShelterApi.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class DogsController : ControllerBase
+  {
+    private readonly AnimalShelterApiContext _db;
+
+    public DogsController(AnimalShelterApiContext db)
+    {
+      _db = db;
+    }
+
+    [HttpGet]
+    public async Task<List<Dog>> Get(string name, string breed, int minimumAge)
+    {
+      IQueryable<Dog> query = _db.Dogs.AsQueryable();
+
+      if (name != null)
+      {
+        query = query.Where(entry => entry.Name == name);
+      }
+
+      if (breed != null)
+      {
+        query = query.Where(entry => entry.Breed == breed);
+      }
+
+      if (minimumAge > 0)
+      {
+        query = query.Where(entry => entry.Age >= minimumAge);
+      }
+
+      return await query.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Dog>> GetDog(int id)
+    {
+      Dog dog = await _db.Dogs.FindAsync(id);
+
+      if (dog == null)
+      {
+        return NotFound();
+      }
+
+      return dog;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Dog>> Post(Dog dog)
+    {
+      _db.Dogs.Add(dog);
+      await _db.SaveChangesAsync();
+      return CreatedAtAction(nameof(GetDog), new { id = dog.DogId }, dog);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(int id, Dog dog)
+    {
+      if (id != dog.DogId)
+      {
+        return BadRequest();
+      }
+
+      _db.Dogs.Update(dog);
+
+      try
+      {
+        await _db.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!DogExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+
+      return NoContent();
+    }
+
+    private bool DogExists(int id)
+    {
+      return _db.Dogs.Any(e => e.DogId == id);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteDog(int id)
+    {
+      Dog dog = await _db.Dogs.FindAsync(id);
+      if (dog == null)
+      {
+        return NotFound();
+      }
+
+      _db.Dogs.Remove(dog);
+      await _db.SaveChangesAsync();
+
+      return NoContent();
+    }
+  }
+}
